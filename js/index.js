a68e40b0154ea5a60f057313bd16365487e46b1b
Janzie21 <nick_janssen21@hotmail.com>
2020-12-09 16:08:03 +0100

    added info Ex-machina

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 const btnRef = document.getElementById('btnGenre');
+const butRef = document.getElementById('btnTrending');
+const btnDataRef = document.getElementById('btnData');
 const listRef = document.getElementById('listGenre');
+const lijstRef= document.getElementById('listTrending');
+const listMovieRef = document.getElementById('filmInfo')
 
 btnRef.addEventListener('click',function() {
     fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=23de8fca6db455ca340aaec8144cf4a3&language=en-US`)
@@ -10,3 +14,19 @@ btnRef.addEventListener('click',function() {
         listRef.innerHTML = data.genres.map((genre)=>`<li>${genre.name}</li>`).join("")
       });
     });
+
+
+btnDataRef.addEventListener('click',function() {
+  fetch(`https://api.themoviedb.org/3/movie/264660?api_key=23de8fca6db455ca340aaec8144cf4a3&language=en-US
+  `)
+  .then(function(resp){
+    return resp.json()
+  })
+  .then(function(data) {
+    listMovieRef.innerHTML = `<h2>${data.original_title}</h2>
+    <p><img src="https://image.tmdb.org/t/p/w500/${data.poster_path}"</p>
+    <p>${data.overview}</p>
+    `;
+  })
+
+});
